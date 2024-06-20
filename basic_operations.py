f0837e0375425bba45bf5c87903dd7974551367f
mathooko <mutuasteven254@gmail.com>
2024-06-20 16:44:28 +0300

    First commit with Basic Arithmetic Exercise

---
+++ basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+sum=number1+number2
+diff=number1-number2
+multiple=number1*number2
+
+print("Addition of 10 and 5 is"sum, \"Subtraction of 10 and is"diff,\"Multiplication of 10 and 5 is " multiple)
+
+
