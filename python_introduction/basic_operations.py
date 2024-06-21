40dba7fa4792586835d6c9a2d1eaa1c0ad22406e
mathooko <mutuasteven254@gmail.com>
2024-06-21 20:08:52 +0300

    Fixing of syntax errors in  basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -4,7 +4,8 @@ sum=number1+number2
 diff=number1-number2
 multiple=number1*number2
 
-print("Addition of 10 and 5 is"sum, \"Subtraction of 10 and is"diff,\"Multiplication of 10 and 5 is " multiple)
+print("Addition of 10 and 5 is", sum, "\nSubtraction of 10 and is",diff,"\nMultiplication of 10 and 5 is " ,multiple)
+
 
 
 
