7644c5315bfa9f1aa768f7eb75addcdf6546fae5
mathooko <mutuasteven254@gmail.com>
2024-06-21 20:46:32 +0300

    Minor rectifications age calculetor

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,5 +1,5 @@
 print("How old are you?")
 currentAge=  int(input())
-age = currentAge+27
-print("In 2050, you will be ",age," years old")
+age = currentAge + 27
+print("In 2050, you will be",age,"years old")
 
