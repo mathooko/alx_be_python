b5688c7480575fec216899e054d573b8e67de536
mathooko <mutuasteven254@gmail.com>
2024-07-03 06:24:53 +0300

    Correction of input statement for multiplication_table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
-number1 = input(int("Enter a number to see its multiplication table:"))
+number1 = int(input("Enter a number to see its multiplication table: "))
 
-for x in range(10):
+for x in range(1, 10):
     print(int(x)*number1)
