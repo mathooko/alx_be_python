865e8471ec0141be7acad5dc6676305f668e3b34
mathooko <mutuasteven254@gmail.com>
2024-07-05 21:53:31 +0300

    correction for pattern drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,8 +2,9 @@ size=int(input("Enter the size of the pattern: "))
 row=0
 
 while row< size:
-    # for item in pInt:
-      for i in range(size):
+    # for item in pInt: does not work
+    #  for row in range(size):check why this does not work
+      for row in range(size):
         x=size*"*"
         print(x)
 #i+=1 Does not work
