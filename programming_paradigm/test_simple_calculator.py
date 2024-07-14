8789fbac13d2d18a41570d1d821a8637d8b9c879
mathooko <mutuasteven254@gmail.com>
2024-07-14 21:19:23 +0300

    test calc correction of method names

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,17 +12,17 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
 
-    def test_addition(self):
+    def test_subtraction(self):
 
         self.assertEqual(self.calc.subtract(2, 3), -1)
         self.assertEqual(self.calc.subtract(2, 1), 1)
 
-    def test_addition(self):
+    def test_multiply(self):
 
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-1, 1), -1)
 
-    def test_addition(self):
+    def test_divide(self):
         
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(1, 1), 1)
