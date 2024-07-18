97280fa361e03b012e19adf6158201611eda9718
mathooko <mutuasteven254@gmail.com>
2024-07-18 12:05:20 +0300

    Addition of codes to main and altering polymorphism class

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -16,20 +16,6 @@ class Circle(Shape):
     def __init__(self,radius):
         super().__init__()
         self.radius=radius
-    def arae(self):
+    def area(self):
         return math.pi * self.radius** 2
-
-from polymorphism_demo import Shape, Rectangle, Circle
-import math
-
-def main():
-    shapes = [
-        Rectangle(10, 5),
-        Circle(7)
-    ]
-
-    for shape in shapes:
-        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
-
-if __name__ == "__main__":
-    main()
+        
