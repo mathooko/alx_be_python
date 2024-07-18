5da3f03b9b3c47dd65f2df2402410adaf97d9176
mathooko <mutuasteven254@gmail.com>
2024-07-18 07:22:24 +0300

    librarry modified

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,46 +1,46 @@
-from library_management import Book, Library
+# from library_management import Book, Library
 
-def main():
-    # Setup a small library
-    library = Library()
-    library.add_book(Book("Brave New World", "Aldous Huxley"))
-    library.add_book(Book("1984", "George Orwell"))
+# def main():
+#     # Setup a small library
+#     library = Library()
+#     library.add_book(Book("Brave New World", "Aldous Huxley"))
+#     library.add_book(Book("1984", "George Orwell"))
 
-    # Initial list of available books
-    print("Available books after setup:")
-    library.list_available_books()
+#     # Initial list of available books
+#     print("Available books after setup:")
+#     library.list_available_books()
 
-    # Simulate checking out a book
-    library.check_out_book("1984")
-    print("\nAvailable books after checking out '1984':")
-    library.list_available_books()
+#     # Simulate checking out a book
+#     library.check_out_book("1984")
+#     print("\nAvailable books after checking out '1984':")
+#     library.list_available_books()
 
-    # Simulate returning a book
-    library.return_book("1984")
-    print("\nAvailable books after returning '1984':")
-    library.list_available_books()
+#     # Simulate returning a book
+#     library.return_book("1984")
+#     print("\nAvailable books after returning '1984':")
+#     library.list_available_books()
 
 # if __name__ == "__main__":
-#     main()
+#      main()
 
-# class Book:
-#     def __init__(self, title,author, _is_checked_out):
+class Book:
+    def __init__(self, title,author, _is_checked_out):
 
-#         self.title=title
-#         self.author=author
-#         self._is_checked_out=_is_checked_out
-#     class Library :
-#         def __init__(self):
+        self.title=title
+        self.author=author
+        self._is_checked_out=_is_checked_out
+    class Library :
+        def __init__(self):
 
-#             self._books= [] 
-#         def add_book(self):
-#              if isinstance(book, Book):#Checks whether object book is an instance of the Book class
-#                 self.books.append(book)
-#         def check_out_book(self,title):
-#             self.title=title
-#         def return_book(self,title):
-#             self.title=title            
-#         def list_available_books(self):
-#                 for book in self.books:
-#                     print(f"{self.title}")
+            self._books= [] 
+        def add_book(self):
+             if isinstance(book, Book):#Checks whether object book is an instance of the Book class
+                self.books.append(book)
+        def check_out_book(self,title):
+            self.title=title
+        def return_book(self,title):
+            self.title=title            
+        def list_available_books(self):
+                for book in self.books:
+                    print(f"{self.title}")
             
--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -32,46 +32,47 @@
 
 #Main for safe divide
 
-import sys
-from robust_division_calculator import safe_divide
+# import sys
+# from robust_division_calculator import safe_divide
 
-def main():
-    if len(sys.argv) != 3:
-        print("Usage: python main.py <numerator> <denominator>")
-        sys.exit(1)
+# def main():
+#     if len(sys.argv) != 3:
+#         print("Usage: python main.py <numerator> <denominator>")
+#         sys.exit(1)
 
-    numerator = sys.argv[1]
-    denominator = sys.argv[2]
+#     numerator = sys.argv[1]
+#     denominator = sys.argv[2]
 
-    result = safe_divide(numerator, denominator)
-    print(result)
-
-if __name__ == "__main__":
-    main()
-
-#Main for library Management
+#     result = safe_divide(numerator, denominator)
+#     print(result)
 
 # if __name__ == "__main__":
 #     main()
 
-# class Book:
-#     def __init__(self, title,author, _is_checked_out):
+#Main for library Management
 
-#         self.title=title
-#         self.author=author
-#         self._is_checked_out=_is_checked_out
-#     class Library :
-#         def __init__(self):
 
-#             self._books= [] 
-#         def add_book(self):
-#              if isinstance(book, Book):#Checks whether object book is an instance of the Book class
-#                 self.books.append(book)
-#         def check_out_book(self,title):
-#             self.title=title
-#         def return_book(self,title):
-#             self.title=title            
-#         def list_available_books(self):
-#                 for book in self.books:
-#                     print(f"{self.title}")
-            
+from library_management import Book, Library
+
+def main():
+    # Setup a small library
+    library = Library()
+    library.add_book(Book("Brave New World", "Aldous Huxley"))
+    library.add_book(Book("1984", "George Orwell"))
+
+    # Initial list of available books
+    print("Available books after setup:")
+    library.list_available_books()
+
+    # Simulate checking out a book
+    library.check_out_book("1984")
+    print("\nAvailable books after checking out '1984':")
+    library.list_available_books()
+
+    # Simulate returning a book
+    library.return_book("1984")
+    print("\nAvailable books after returning '1984':")
+    library.list_available_books()
+
+if __name__ == "__main__":
+     main()   
