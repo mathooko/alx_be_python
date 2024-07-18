ee1e9241731b4ab442ac1e1be9f19bc0d5747ff9
mathooko <mutuasteven254@gmail.com>
2024-07-18 11:30:42 +0300

    Removal of self

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -5,13 +5,17 @@
 #          self.numerator=numerator
 #          self.denominator=denominator
 
-def safe_divide(self, numerator,denominator):
+def safe_divide(numerator,denominator):
+        #Ask Why remove self in this instance
         try:
-            self.num = float(numerator)
-            self.denom = float(denominator)
+            num = float(numerator)
+            denom = float(denominator)
             result = num/denom 
             return f"The result of the division is {result}"
         except ZeroDivisionError:
             return "Error: Cannot divide by zero."
         except ValueError:
             return "Error: Please enter numeric values only."
+
+
+#Ask Why remove self in this instance
