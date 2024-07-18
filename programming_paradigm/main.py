b2ddc1a02c88a8087f19a7be3591c5dc7adf8e6f
mathooko <mutuasteven254@gmail.com>
2024-07-18 07:02:57 +0300

    addition of commented main for library

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -1,3 +1,5 @@
+#Bank Account
+
 # import sys
 # from bank_account import BankAccount
 
@@ -28,6 +30,8 @@
 # if __name__ == "__main__":
 #     main()
 
+#Main for safe divide
+
 import sys
 from robust_division_calculator import safe_divide
 
@@ -44,3 +48,30 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+#Main for library Management
+
+# if __name__ == "__main__":
+#     main()
+
+# class Book:
+#     def __init__(self, title,author, _is_checked_out):
+
+#         self.title=title
+#         self.author=author
+#         self._is_checked_out=_is_checked_out
+#     class Library :
+#         def __init__(self):
+
+#             self._books= [] 
+#         def add_book(self):
+#              if isinstance(book, Book):#Checks whether object book is an instance of the Book class
+#                 self.books.append(book)
+#         def check_out_book(self,title):
+#             self.title=title
+#         def return_book(self,title):
+#             self.title=title            
+#         def list_available_books(self):
+#                 for book in self.books:
+#                     print(f"{self.title}")
+            
