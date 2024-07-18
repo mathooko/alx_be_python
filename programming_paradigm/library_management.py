8198b9ae3d602331d6ede3cf1b371f7f2b6d6587
mathooko <mutuasteven254@gmail.com>
2024-07-18 07:26:41 +0300

    Proper implementation of library class

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -29,7 +29,7 @@ class Book:
         self.title=title
         self.author=author
         self._is_checked_out=_is_checked_out
-    class Library :
+class Library :
         def __init__(self):
 
             self._books= [] 
