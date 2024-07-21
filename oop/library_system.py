5fd2810e5954fb8e6a23401b644d8d0f158d77ae
mathooko <mutuasteven254@gmail.com>
2024-07-21 18:48:10 +0300

    Library system main's commented, book class return statement rectified

--- oop/library_system.py
+++ oop/library_system.py
@@ -33,18 +33,18 @@ class Library:
        return f"{self.title} is written by {self.author} {[str (book)for book in self.books]}"
             # print(f"{self.title} is written by {self.author}")
     
-my_library = Library()
+# my_library = Library()
 
-# Create instances of each type of book
-classic_book = Book("Pride and Prejudice", "Jane Austen")
-digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
-paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
+# # Create instances of each type of book
+# classic_book = Book("Pride and Prejudice", "Jane Austen")
+# digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
+# paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
 
-# Add books to the library
-my_library.add_book(classic_book)
-my_library.add_book(digital_novel)
-my_library.add_book(paper_novel)
+# # Add books to the library
+# my_library.add_book(classic_book)
+# my_library.add_book(digital_novel)
+# my_library.add_book(paper_novel)
 
-# List all books in the library
-my_library.list_books()
+# # List all books in the library
+# my_library.list_books()
 
