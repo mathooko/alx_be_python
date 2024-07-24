a617df2991b0d8886adf059cbf7730127423c4b0
mathooko <mutuasteven254@gmail.com>
2024-07-24 18:34:32 +0300

    Blessings implementation. Still not working

--- oop/library_system.py
+++ oop/library_system.py
@@ -12,19 +12,19 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size=file_size
         def __str__(self):
-            return f"Book: {self.title} by {self.author}, File Size: {self.file_size}"
+            return f"{super().__str__()} File Size: {self.file_size}"
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count=page_count
         def __str__(self):
-            return f"Book: {self.title} by {self.author}, Page Count: {self.page_count}"
+            return f"{super().__str__()}, Page Count: {self.page_count}"
 
 class Library:
-    # books = []
-    def __init__(self):
+    books = []
+    # def __init__(self):
         
-        self.books = []
+    #     self.books = []
         # self.name=name
 
     def add_book(self, book):
