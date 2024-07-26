cff5778098c3eeff7e888f761fe0ae48bfaf2e30
mathooko <mutuasteven254@gmail.com>
2024-07-26 19:07:04 +0300

    Arithmetic operators rectified

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,3 @@
-def main():
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-    result = perform_operation(num1, num2, operation)
-    print(f"Result: {result}")
-if __name__ == "__main__":
-    main()
-
-
 #     fNumber = int(input(def perform_operation():"Enter the first number: "))
 # sNumber = int(input("Enter the second number: "))
 
@@ -16,17 +5,21 @@ if __name__ == "__main__":
 def perform_operation(num1, num2, operation):
 
     
-        if operation=="+":
-            print(result=num1+num2)
-        elif operation=="-":
-            print(result=num1-num2)
-        elif operation=="*":
-            print(result=num1*num2)
-        elif operation== "/":
+        if operation=="add":
+            return num1+num2
+            # print(f"Result: {result}")# Will return None
+             
+        elif operation=="subtract":
+            return num1-num2
+           
+        elif operation=="multiply":
+            return num1*num2
+           
+        elif operation== "divide":
             if num2 == 0:
-                return print("Number cannot divide 0")
+                return "Number cannot divide 0"
             else:
                 result=num1/num2
-                return print(result)
+                return result
         else:
-            print("Invalid input")
+            return "Invalid input"
