98369e4b2bb191afd3f644cf5e9c82a28a75b8cf
mathooko <mutuasteven254@gmail.com>
2024-07-26 15:19:49 +0300

    Fixing of indentation in str function

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,20 +4,20 @@ class Book:
         self.title= title
         self.author=author
 
-        def __str__(self):
+    def __str__(self):
             return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size=file_size
-        def __str__(self):
+    def __str__(self):
             return f"{super().__str__()} File Size: {self.file_size}"
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count=page_count
-        def __str__(self):
+    def __str__(self):
             return f"{super().__str__()}, Page Count: {self.page_count}"
 
 class Library:
