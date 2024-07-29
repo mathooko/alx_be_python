5ce061b0fd0527a6bbaa4bf3c7c095717a33637d
mathooko <mutuasteven254@gmail.com>
2024-07-29 12:55:58 +0300

    Book class working properly addition of main inside class static methods

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -9,3 +9,17 @@ class Calculator:
     def multiply(cls, a, b):
         print(f"Calculation type: {cls.calculation_type}")
         return a * b
+    
+# # from class_static_methods_demo import Calculator
+
+# def main():
+#     # Using the static method
+#     sum_result = Calculator.add(10, 5)
+#     print(f"The sum is: {sum_result}")
+
+#     # Using the class method
+#     product_result = Calculator.multiply(10, 5)
+#     print(f"The product is: {product_result}")
+
+# if __name__ == "__main__":
+#     main()
