c1905874c387559535ae7d46cf37af4e230c085c
mathooko <mutuasteven254@gmail.com>
2024-08-02 06:14:39 +0300

    Implementation of value error

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -9,8 +9,12 @@ def convert_to_celsius(fahrenheit):
   
 # def main():
 
-temp1=int(input("Enter the temperature to convert:"))
+try: 
+    temp1=int(input("Enter the temperature to convert:"))  
+except ValueError:
+    print("Invalid temperature. Please enter a numeric value.")
 temperatureUnit=input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+
 
 if temperatureUnit=="F":
     convert = convert_to_celsius(temp1)
@@ -28,6 +32,38 @@ elif temperatureUnit=="C":
     
 else:
     print("Invalid input")
+
+
+
+
+# This will not work as the exception does not test for the input from temp1 rather it is the one from temperature unit
+
+# temp1=int(input("Enter the temperature to convert:"))
+
+# temperatureUnit=input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+
+# try:
+#     if temperatureUnit=="F":
+#         convert = convert_to_celsius(temp1)
+#         print(f"{temp1}\u00B0F is {convert}\u00B0C")
+        
+#         # convert_to_celsius(temp1)#temp1 is input
+#         # fahrenheit=temp1
+        
+#     elif temperatureUnit=="C":
+#         convert = convert_to_fahrenheit(temp1)
+#         print(f"{temp1}\u00B0C is {convert}\u00B0F")
+        
+#         # convert_to_fahrenheit(celsius)
+#         # celsius=temp1
+        
+#     else:
+#         print("Invalid input")
+
+# except ValueError:
+#     print("Invalid temperature. Please enter a numeric value.")
+
+
     
 # main()
 
